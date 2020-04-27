73cba4e0f151752105e474acb2aeb8220a40456b
Eugene Dimchenko <edimchenko@gmail.com>
2020-04-27 11:24:52 +0100

    deleted migrations and app.db

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -4,6 +4,7 @@ from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
 from app.models import User
 
 
+
 class LoginForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
